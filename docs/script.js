953c3c52a431490d08b78cfe9b5a9c8cd769683c
Luana RR <luanarabelo2@gmail.com>
2026-05-19 19:25:03 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,9 @@
-
+document.querySelectorAll('article, .timeline div').forEach((el, index) => {
+  el.style.opacity = '0';
+  el.style.transform = 'translateY(18px)';
+  setTimeout(() => {
+    el.style.transition = 'all .6s ease';
+    el.style.opacity = '1';
+    el.style.transform = 'translateY(0)';
+  }, index * 120);
+});
